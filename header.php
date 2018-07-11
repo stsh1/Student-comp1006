d02dbac45a8716a3dfafce0a27223cc611e1947b
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-11 11:19:26 +0530

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<ul>
 			<li><a href="#">Home</a></li>
 			<li><a href="#">About Us</a></li>
-			<li><a href="#">Register</a></li>
+			<li><a href="view.php">Register</a></li>
 			<li><a href="#">Contact Us</a></li>
 			<!--abc-->
 		</ul>
